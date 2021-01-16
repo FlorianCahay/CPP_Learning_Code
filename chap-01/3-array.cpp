543e7962009818e9d0a32261e73fa0297c13775c
FlorianCahay <38866797+FlorianCahay@users.noreply.github.com>
2021-01-17 00:03:56 +0100

    chap-01

--- chap-01/3-array.cpp
+++ chap-01/3-array.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
+#include <vector>
 
-int main()
+int main(int argc, char** argv)
 {
-    int values[] = { 0, 1, 2, 3 };
+    if (argc <= 1) {
+        std::cerr << "Error: we need one argument" << std::endl;
+        return -1;
+    }
+    int length = std::stoi(argv[1]);
+    if (length <= 0) {
+        std::cerr << "Error: we need positive value for array size" << std::endl;
+        return -1;
+    }
 
-    for (int value : values)
-    {
+    /* int* values = new int[length] {};
+
+    for (auto i = 0; i < length; i++) {
+        values[i] = i+1;
+    }
+
+    for (auto i = 0; i < length; i++) {
+        std::cout << values[i] << std::endl;
+    }
+
+    delete[] values; */
+
+    std::vector<int> values;
+
+    for (auto i = 0; i < length; i++) {
+        values.emplace_back(i+1);
+    }
+
+    for (auto value : values) {
         std::cout << value << std::endl;
     }
 
--- chap-01/4-calculator.cpp
+++ chap-01/4-calculator.cpp
@@ -2,65 +2,96 @@
 #include <string>
 #include <vector>
 
-int main(int argc, char** argv)
-{
-    // Parsing program parameters.
+int add(const std::vector<int> values) {
+    auto result = 0;
+    for (auto v : values)
+    {
+        result += v;
+    }
+    return result;
+}
+
+int mul(const std::vector<int> values) {
+    auto result = 1;
+    for (auto v : values)
+    {
+        result *= v;
+    }
+    return result;
+}
+
+int sub(const std::vector<int> values) {
+    auto result = values[0];
+    for (auto i = 1u; i < values.size(); ++i)
+    {
+        result -= values[i];
+    }
+    return result;
+}
+
+bool parse_params(char& op, std::vector<int>& values, int argc, char** argv) {
     if (argc < 2)
     {
         std::cerr << "Expected operator as first argument." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::string op = argv[1];
+    std::string op_str = argv[1];
+    std::cout << op_str << std::endl;
 
-    if (op != "+" && op != "*" && op != "-")
+    if (op_str != "+" && op_str != "*" && op_str != "-")
     {
         std::cerr << "Expected operator to be '+', '*' or '-'." << std::endl;
-        return -1;
+        return false;
     }
 
-    std::vector<int> values;
+    op = op_str[0];
+
     for (auto arg_i = 2; arg_i < argc; ++arg_i)
     {
         auto value = std::stoi(argv[arg_i]);
         values.emplace_back(value); 
     }
 
+    if (op == '-' && values.empty()) {
+        std::cerr << "Error: operator '-' expects at least one operand to substract from." << std::endl;
+        return -1;
+    }
+
+    return true;
+}
+
+int compute_result(const char op, const std::vector<int> values) {
+    switch (op) {
+        case '+':
+            return add(values);
+        case '*':
+            return mul(values);
+        case '-':
+            return sub(values);
+        default:
+            return 0;
+    }
+}
+
+void display_result(int result) {
+    std::cout << "Result is " << result << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    // Parsing program parameters.
+    char op = '?';
+    std::vector<int> values;
+    if (!parse_params(op, values, argc, argv)) {
+        return -1;
+    }
+
     // Process operation, depending on the operator.
-    auto result = 0;
-
-    if (op == "+")
-    {
-        for (auto v : values)
-        {
-            result += v;
-        }
-    }
-    else if (op == "*")
-    {
-        result = 1;
-        for (auto v : values)
-        {
-            result *= v;
-        }
-    }
-    else if (op == "-")
-    {
-        if (values.empty())
-        {
-            std::cerr << "Operator '-' expects at least one operand to substract from." << std::endl;
-            return -1;
-        }
-
-        result = values[0];
-        for (auto i = 1u; i < values.size(); ++i)
-        {
-            result -= values[i];
-        }
-    }
+    auto result = compute_result(op, values);
 
     // Output result.
-    std::cout << "Result is " << result << std::endl;
+    display_result(result);
 
     return 0;
 }
